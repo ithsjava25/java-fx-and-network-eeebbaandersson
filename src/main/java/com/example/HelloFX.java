dbbb53f9bbb42a6d49b88e220c394089a5fc9e48
Ebba Andersson <eeebbaandersson@gmail.com>
2025-10-31 17:47:01 +0100

    start

--- src/main/java/com/example/HelloFX.java
+++ src/main/java/com/example/HelloFX.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
@@ -12,8 +13,8 @@ public class HelloFX extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloFX.class.getResource("hello-view.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root, 640, 480);
-        stage.setTitle("Hello MVC");
+        Scene scene = new Scene(root, 580, 740);
+        stage.setTitle("JavaFX ChatApplication");
         stage.setScene(scene);
         stage.show();
     }
