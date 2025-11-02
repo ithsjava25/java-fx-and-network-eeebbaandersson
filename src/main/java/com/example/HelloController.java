8a6eacace94c9289a65181d9b8e6a1418d8c0f8f
Ebba Andersson <eeebbaandersson@gmail.com>
2025-11-02 17:16:23 +0100

    Created styling for user/sender-bubble and added basic methods to add/sendMessage

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -9,6 +15,16 @@ import javafx.scene.control.Label;
 public class HelloController {
 
     private final HelloModel model = new HelloModel();
+
+
+    @FXML
+    private ScrollPane chatScrollPane;
+
+    @FXML
+    private VBox messageArea;
+
+    @FXML
+    private TextField inputField;
 
     @FXML
     private Label messageLabel;
@@ -19,5 +35,44 @@ public class HelloController {
         if (messageLabel != null) {
             messageLabel.setText(model.getGreeting());
         }
+    }
+
+    //Funktion för att skicka meddelande när användaren trycker enter
+    @FXML
+    public void sendMessage(ActionEvent actionEvent) {
+        String messageText = inputField.getText().trim();
+
+        if(!messageText.isEmpty()) {
+            addMessage(messageText, true);
+            inputField.clear();
+
+            //Tillfälligt svar från "bot"
+            addMessage("Jag fick ditt meddelande: " + messageText, false);
+        }
+    }
+
+    private void addMessage(String text, boolean isUser) {
+        HBox messageContainer = createMessageBubble(text, isUser);
+
+        messageArea.getChildren().add(messageContainer);
+
+    }
+
+    private HBox createMessageBubble(String text, boolean isUser) {
+        Label messageLabel = new Label(text);
+        messageLabel.setWrapText(true);
+        messageLabel.setMaxWidth(250);
+
+        HBox container = new HBox();
+        container.getChildren().add(messageLabel);
+
+        if (isUser) {
+            messageLabel.getStyleClass().add("user-bubble");
+            container.setAlignment(Pos.CENTER_RIGHT);
+        } else {
+            messageLabel.getStyleClass().add("sender-bubble");
+            container.setAlignment(Pos.CENTER_LEFT);
+        }
+        return container;
     }
 }
