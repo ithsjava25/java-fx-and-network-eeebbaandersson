476655e3557ed01e79d1218e71dc9b4b4e403c15
Ebba Andersson <eeebbaandersson@gmail.com>
2025-11-03 20:30:51 +0100

    Added NtfyMessagesDto-record and added dependencies to pom.xml-file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.annotation;
 
     opens com.example to javafx.fxml;
     exports com.example;
